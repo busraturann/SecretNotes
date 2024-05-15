50b34dc3b8000ec79d5232319647c19ecf2985bd
Büşra Turan <busra99turan@gmail.com>
2024-05-15 17:57:19 +0300

    completed

---
+++ main.py
@@ -0,0 +1,104 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+
+# obscure
+# apply cryptography with vigenere ciphher
+# https://stackoverflow.com/a/38223403
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+def savencrypt():
+    title = title_entry.get()
+    message = secret_box.get("1.0", END)
+    master_secret = key_entry.get()
+
+    if len(title) == 0 or len(message) == 0 or len(master_secret) == 0:
+        messagebox.showinfo(title="Error!", message="Please enter all info!")
+    else:
+        # encryption
+        message_encrypted = encode(master_secret, message)
+
+        try:
+            with open("mysecret.txt", "a") as data_file:
+                data_file.write(f"\n{title}\n{message_encrypted}")
+        except FileNotFoundError:
+            with open("mysecret.txt", "w") as data_file:
+                data_file.write(f"\n{title}\n{message_encrypted}")
+        finally:
+            title_entry.delete(0, END)
+            secret_box.delete("1.0", END)
+            key_entry.delete(0, END)
+
+
+def decrypt_notes():
+    message_encrypted = secret_box.get("1.0", END)
+    master_secret = key_entry.get()
+
+    if len(message_encrypted) == 0 or len(master_secret) == 0:
+        messagebox.showinfo(title="Error!", message="Please enter all information.")
+    else:
+        try:
+            decrypted_message = decode(master_secret, message_encrypted)
+            secret_box.delete("1.0", END)
+            secret_box.insert("1.0", decrypted_message)
+        except:
+            messagebox.showinfo(title="Error!", message="Please make sure of encrypted info.")
+
+
+window = Tk()
+window.title("Secret Notes")
+window.config(pady=30, padx=30)
+
+
+# logo
+picture = PhotoImage(file="topsecret.png")
+label1 = Label(image=picture)
+label1.pack(pady=50)
+
+
+# enter your title
+title_label = Label(text="Enter your title", font=25)
+title_label.pack()
+title_entry = Entry(width=40)
+title_entry.pack()
+
+
+
+# enter your secret
+secret_label = Label(text="Enter your secret", font=25)
+secret_label.pack()
+secret_box = Text(width=50, height=25)
+secret_box.pack()
+
+
+# enter your master key
+key_label = Label(text="Enter master key", font=25)
+key_label.pack()
+key_entry = Entry(width=30, show='*')
+key_entry.pack()
+
+
+# save & encrypt button
+save_button = Button(text="Save & Encrypt", command=savencrypt)
+save_button.pack()
+
+# decrypt button
+decr_button = Button(text="Decrypt", command=decrypt_notes)
+decr_button.pack()
+
+window.mainloop()
